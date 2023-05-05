8bc676d8586db8c514bf30b25f3f67591387bcbb
Dev_MiguelPinheiro <82911261+mikas-hash@users.noreply.github.com>
2023-05-05 08:50:38 -0300

    criando primeiras telas

---
+++ Godzilla/src/main/java/com/mycompany/projeto/godzilla/Godzilla.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.projeto.godzilla;
+
+/**
+ *
+ * @author equipeGodzilla
+ */
+public class Godzilla {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
