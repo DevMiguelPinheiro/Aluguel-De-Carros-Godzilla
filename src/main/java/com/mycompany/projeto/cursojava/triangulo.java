a7c52c160e71b97268b333bc8651b246b5393936
Dev_MiguelPinheiro <leitemiguelpinheiro@gmail.com>
2023-05-17 13:23:02 -0300

    criacao da classe triangulo

---
+++ src/main/java/com/mycompany/projeto/cursojava/triangulo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projeto.cursojava;
+
+/**
+ *
+ * @author migue
+ */
+public class triangulo {
+     double a;
+     double b;
+     double c;
+}
