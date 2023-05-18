642051686fafab99589a740d50386919ca9e4c03
Dev_MiguelPinheiro <leitemiguelpinheiro@gmail.com>
2023-05-18 10:47:41 -0300

    exercicio de fixacao 1

--- src/main/java/com/mycompany/projeto/cursojava/CursoJava.java
+++ src/main/java/com/mycompany/projeto/cursojava/CursoJava.java
@@ -3,46 +3,41 @@
  */
 
 package com.mycompany.projeto.cursojava;
-import com.mycompany.projeto.entidades.NewClass;
+import com.mycompany.projeto.entidades.retangle;
 import java.util.Locale;
 import java.util.Scanner;
 
 
 /**
-Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
-válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
-possui a maior área.
-A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
-seguinte (fórmula de Heron):
+Fazer um programa para ler os valores da largura e altura 
+de um retângulo. Em seguida, mostrar na tela o valor de 
+sua área, perímetro e diagonal. Usar uma classe como 
+mostrado no projeto ao lado.
+* 
+* .format() serve pra formatar o  resultado final como casas decimais
  */
 public class CursoJava {
 
     public static void main(String[] args)  {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        NewClass x,y;
-        x = new NewClass();
-        y = new NewClass();
-        System.out.println("Enter the measures of triangle X: ");
+        retangle x;
+
+        x = new retangle();
+        System.out.println("Enter the width of retangle X: ");
         x.a = sc.nextDouble();
+        System.out.println("Enter the height of retangle X: ");
         x.b = sc.nextDouble();
-        x.c = sc.nextDouble();
-        System.out.println("Enter the measures of triangle Y: ");
-        y.a = sc.nextDouble();
-        y.b= sc.nextDouble();
-        y.c= sc.nextDouble();
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
-        System.out.printf("Triangle X area: %.4f%n", areaX);
-        System.out.printf("Triangle Y area: %.4f%n", areaY);
-        if (areaX > areaY) {
-        System.out.println("Larger area: X");
-        }
-        else {
-        System.out.println("Larger area: Y");
-        }
+  
+     
+        double areaX = x.area();
+        double perimeterX = x.perimeter();
+        double diagonalX = x.diagonal();
+        
+        System.out.printf("retangle X area: %.4f%n", areaX);
+        System.out.printf("retangle X perimeter: %.4f%n", perimeterX);
+        System.out.printf("retangle X perimeter: %.4f%n", diagonalX);
+ 
         sc.close();
     }
 }
